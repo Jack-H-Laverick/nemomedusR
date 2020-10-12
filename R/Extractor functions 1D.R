b1a6f295ee6feba9e7df51d09cc6a2d0fd14037f
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-10-12 12:04:18 +0100

    bug fix.

--- R/Extractor functions 1D.R
+++ R/Extractor functions 1D.R
@@ -78,13 +78,13 @@ get_grid_W_1D   <- function(filename, date, x, y) {
   
   print(stringr::str_glue("{filename} Extracting Vertical Velocity, Vertical Eddy Diffusivity"))
   nc_raw <- ncdf4::nc_open(filename)                                # Open up a netcdf file to see it's raw contents (var names)
-  nc_vel <- ncdf4::ncvar_get(nc_raw, "vovercrtz", start=c(x,y,1,1), count=c(1,1,-1,-1))          # Extract an array of velocities
+  nc_vel <- ncdf4::ncvar_get(nc_raw, "vovecrtz", start=c(x,y,1,1), count=c(1,1,-1,-1))          # Extract an array of velocities
   nc_dif <- ncdf4::ncvar_get(nc_raw, "votkeavt", start=c(x,y,1,1), count=c(1,1,-1,-1))           # Extract an array of diffusivity
   ncdf4::nc_close(nc_raw)                                                      # You must close an open netcdf file when finished to avoid data loss
   
   all <- data.frame(                                                                # Bind as columns
-    vel = nc_vel,
-    Chlorophyll = nc_dif,
+    Vertical_velocity = nc_vel,
+    Vertical_diffusivity = nc_dif,
     Date = date) %>% 
     rownames_to_column(var="Depth")
   return(all)
