4b45a7e4ef92db71549cad827f72514ea3dcb154
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-10-12 12:32:21 +0100

    bug hunt.

--- R/Extractor functions 1D.R
+++ R/Extractor functions 1D.R
@@ -248,7 +248,7 @@ if(analysis == "1D") {
   Month <- split(data, f = list(data$Type)) %>%                             # Split out the files for this month by type, so they can be averaged together
     purrr::map(type_in_month, analysis = analysis, ...) %>%                 # Pull a whole month of data from a single file type
     do.call(cbind, .) %>%                                                   # Join together all the data packets
-    saveRDS(., file = stringr::str_glue("{out_dir}/NM.{stringr::str_sub(.$date[1], start = 5, end = 6)}.{stringr::str_sub(.$date[1], start = 7, end = 8)}.rds")) # save out a data object for one whole month
+    saveRDS(., file = stringr::str_glue("{out_dir}/NM.{stringr::str_sub(.$Date[1], start = 5, end = 6)}.{stringr::str_sub(.$Date[1], start = 7, end = 8)}.rds")) # save out a data object for one whole month
 }
   
 if(!analysis %in% c("StrathE2E", "1D")) {
