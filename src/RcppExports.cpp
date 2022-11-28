c9f319076e86c590c0f9acb26b2a77038b5266f9
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-11-28 10:57:30 +0000

    docs: rebuild site.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // array_w_mean
 NumericVector array_w_mean(arma::cube array, DataFrame scheme);
 RcppExport SEXP _nemomedusR_array_w_mean(SEXP arraySEXP, SEXP schemeSEXP) {
