17eb5ca18752dadea1d908d339f0675f1fbfdd79
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-11-28 10:24:40 +0000

    feat: add collect analysis

--- R/wrappers.R
+++ R/wrappers.R
@@ -20,18 +20,23 @@
 #' Also, working on independent packets of data means we can parallelise any data processing for speed.
 #'
 #' @param data A dataframe containing the metadata of multiple netcdf files.
-#' @param analysis A text string dictating the type of summary to create. Currently supported summaries include "StrathE2E" & "1D".
+#' @param analysis A text string dictating the type of summary to create. Deprecated, supports "StrathE2E", "1D", & "slabR" for back compatibility.
+#' @param time_op A text string dictating the type of summary to create. Currently supported summaries include "mean" & "collect". When using time_op all spatial summaries are controlled by slabR schemes.
 #' @param ... Additional arguments passed to the relevant `get_*` function.
 #' @return The function returns a dataframe if concatenating, or a matrix if averaging, containing the spatial summaries
 #' of NEMO-MEDUSA model output.
 #' @export
 
-temporal_operation <- function(data, analysis, ...) {
+temporal_operation <- function(data, analysis = NULL, time_op = NULL, ...) {
   
-  if(!analysis %in% c("StrathE2E", "1D", "slabR")) {
+  if(length(analysis > 0)) {  
+  if(!analysis %in% c("StrathE2E", "1D", "slabR") & !is.null(analysis)) {
     stop("Whoops, I haven't coded that analysis yet. Did you want a StrathE2E, slabR, or a 1D summary?")}
   
-  get <- match.fun(paste0("get_", data[1,"Type"], analysis))                # Change the extracting function based on file contents
+  if(!is.null(analysis) & !is.null(time_op)) {
+    stop("Use either 'analysis' or 'time_op' not both.")}
+
+    get <- match.fun(paste0("get_", data[1,"Type"], analysis))                # Change the extracting function based on file contents
   
   if(analysis %in% c("StrathE2E", "slabR")) {
 
@@ -45,8 +50,27 @@ temporal_operation <- function(data, analysis, ...) {
     temporal_summary <- purrr::map2(data$filename, data$date, get, ...) %>% # Extract the contents of each file
       purrr::map(as.data.frame) %>%                                 
       data.table::rbindlist()                                               # Fast row bind for the data frames
-  } 
+  }} 
 
+  if(length(time_op > 0)) {  
+    
+    get <- match.fun(paste0("get_", data[1,"Type"], "slabR"))               # Change the extracting function based on file contents
+    
+  if(time_op == "mean") {
+    
+    temporal_summary <- purrr::map2(data$Path, data$File, get, ...) %>% # Summarise the contents of each file
+      abind::abind(along = 3) %>%                                 # Stack matrices behind each other
+      rowMeans(dims = 2)                                          # Quickly take the mean for all variables in the month
+  }
+  
+  if(time_op == "collect") {
+    
+    temporal_summary <- purrr::map2(data$Path, data$File, get, ...) %>% # Extract the contents of each file
+      purrr::map(as.data.frame) %>%                                 
+      purrr::map2(data$Date, cbind) %>%                                     # Add the specific date to each file
+      data.table::rbindlist()                                               # Fast row bind for the data frames
+  }}
+  
   return(temporal_summary)
 }
 
@@ -72,7 +96,8 @@ temporal_operation <- function(data, analysis, ...) {
 #' Also, working on independent monthly packets of data means we can parallelise any data processing for speed.
 #'
 #' @param data a dataframe containing the metadata of multiple netcdf files from a common month.
-#' @param analysis a character string choosing the summary operation to apply.
+#' @param analysis A text string dictating the type of summary to create. Deprecated, supports "StrathE2E", "1D", & "slabR" for back compatibility.
+#' @param time_op A text string dictating the type of summary to create. Currently supported summaries include "mean" & "collect". When using time_op all spatial summaries are controlled by slabR schemes.
 #' @param crop on the way out with the StrathE2E analysis type.
 #' @param out_dir a filepath to the directory to save summarised files in.
 #' @param summary a dataframe of metadata to bind to summarised rows. The summary should be arranged by increasing 
@@ -82,15 +107,19 @@ temporal_operation <- function(data, analysis, ...) {
 #' \strong{all} the variables of interest in NEMO-MEDUSA outputs.
 #' @family NEMO-MEDUSA variable extractors
 #' @export
-NEMO_MEDUSA <- function(data, analysis, out_dir = "./Objects/Months", crop = NULL, summary = NULL, ...) {
+NEMO_MEDUSA <- function(data, analysis = NULL, time_op = NULL, out_dir = "./Objects/Months", crop = NULL, summary = NULL, ...) {
 
-  if(!analysis %in% c("StrathE2E", "1D", "slabR")) {
+  if(length(analysis > 0)) {  
+  if(!analysis %in% c("StrathE2E", "1D", "slabR") & !is.null(analysis)) {
     stop("Whoops, I haven't coded that analysis yet. Did you want a StrathE2E, slabR, or 1D summary?")}
 
+  if(!is.null(analysis) & !is.null(time_op)) {
+    stop("Use either 'analysis' or 'time_op' not both.")}
+  
   if(analysis == "StrathE2E") {
     Month <- data[1,5] ; Year <- data[1,4]                                    # Pull date
     
-    print("Consider using slabR with scheme_strathE2E instead of StrahE2E")
+    print("Consider using slabR with scheme_strathE2E instead of StrathE2E")
     
     timestep <- split(data, f = list(data$Type)) %>%                          # Split out the files for this month by type, so they can be averaged together
       purrr::map(temporal_operation, analysis = analysis, ...) %>%            # Pull a whole month of data from a single file type
@@ -130,5 +159,24 @@ NEMO_MEDUSA <- function(data, analysis, out_dir = "./Objects/Months", crop = NUL
       mutate(Month = stringr::str_sub(Date, start = 5, end = 6),
              Year = stringr::str_sub(Date, start = 1, end = 4)) %>% 
       saveRDS(file = paste0(out_dir, "/NM.", .$Month[1], ".", .$Year[1], ".rds")) # save out a data object for one whole month
-  }
+  }}
+  
+  if(length(time_op > 0)) {  
+    
+  if(time_op == "collect") {
+    Month <- data[1,5] ; Year <- data[1,4]                                    # Pull date
+    
+    timestep <- split(data, f = list(data$Type)) %>%                          # Split out the files for this month by type, so they can be averaged together
+      purrr::map(temporal_operation, time_op = "collect", ...)                # Pull a whole month of data from a single file type
+    
+    if(length(timestep) > 1) {timestep <- do.call(cbind, timestep)} else {    # Join together all the data packets if there are multiple file types
+      timestep <- timestep[[1]] }                                             # If there's only one file type expose the output as a matrix
+    
+    timestep <- as.data.frame(timestep) %>% 
+      cbind(summary) %>%                                                      # Add coordinates and depth labels
+      mutate(Year = as.integer(Year),                                         # Add time
+             Month =  as.integer(Month)) %>%
+      saveRDS(file = paste0(out_dir, "/NM.", Month, ".", Year, ".rds")) # save out a data object for one whole month
+  }}
+  
 }
